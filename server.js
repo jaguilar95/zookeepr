5b4aa0caf9a6ac22c9c356577a5f5a05d3e96ba9
Leonardo <josue.aguilar1995@gmail.com>
2022-05-25 20:10:37 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -4,7 +4,26 @@ const app = express();
 const { animals } = require("./data/animals.json");
 
 function filterByQuery(query, animalsArray) {
+  let personalityTraitsArray = [];
   let filteredResults = animalsArray;
+
+  if (query.personalityTraits) {
+    // Save personalityTraits as a dedicated array.
+    // If personalityTraits is a string, place it into a new array and save.
+    if (typeof query.personalityTraits === "string") {
+      personalityTraitsArray = [query.personalityTraits];
+    } else {
+      personalityTraitsArray = query.personalityTraits;
+    }
+    // Loop through each trait in the personalityTraits array:
+    personalityTraitsArray.forEach((trait) => {
+      // Check the trait against each animal in the filteredResults array.
+      filteredResults = filteredResults.filter(
+        (animal) => animal.personalityTraits.indexOf(trait) !== -1
+      );
+    });
+  }
+
   if (query.diet) {
     filteredResults = filteredResults.filter(
       (animal) => animal.diet === query.diet
@@ -20,6 +39,7 @@ function filterByQuery(query, animalsArray) {
       (animal) => animal.name === query.name
     );
   }
+
   return filteredResults;
 }
 
